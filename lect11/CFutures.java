b6433e9f578f7766275737d1a8bb9681c1bea7c3
Ong Yu-He <yuhembp@Ongs-MacBook-Pro.local>
2018-04-18 00:22:40 +0800

    Added lect11

---
+++ lect11/CFutures.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    // 1i
+    D d = CompletableFuture.completedFuture(a)
+            .thenApplyAsync(a -> f(a))
+            .thenApplyAsync(b -> g(b))
+            .thenApplyAsync(c -> h(c))
+            .join();
+    
+    // 1ii
+    CompletableFuture.supplyAsync(() -> f())
+                    .thenApplyAynsc(b -> g(b))
+                    .thenAcceptAsync(c -> h(c))
+                    .join();
+
+    // 1iii
+    CompletableFuture<B> bFuture = CompletableFuture.completedFuture(a)
+                                    .thenApplyAsync(a -> f(a));
+    CompletableFuture<C> cFuture = bFuture.thenApplyAsync(b -> g(b));
+    CompletableFuture<D> dFuture = bFuture.thenApplyAsync(b -> h(b));
+    E e = cFuture.thenCombineAsync(dFuture, (c, d) -> i(c,d))
+                            .join();   
+}
